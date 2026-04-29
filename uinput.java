c32a5dec58f3b504806e6d45b1baf15f7252d727
Harini03-11-2004 <harinim0311@gmail.com>
2026-04-29 15:54:11 +0530

    user input

---
+++ uinput.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class uinput {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n;
+        do{
+             n=s.nextInt();
+             System.out.println("User Enter:"+n);
+             
+        }
+       while(n!=0);
+      
+    }
+}
+
---
+++ whl.java
@@ -0,0 +1,13 @@
+ import java.util.*;
+ class whl {
+    public static void main(String[] args) {
+        Scanner s =new Scanner(System.in);
+        int n;
+        n=s.nextInt();
+        while(n!=0)
+        {
+            System.out.println(n);
+            n=s.nextInt();
+        }
+    }
+}
