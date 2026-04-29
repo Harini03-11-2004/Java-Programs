04c2707936a79c197fa03226d4be6ce35e11075f
Harini03-11-2004 <harinim0311@gmail.com>
2026-04-29 21:54:26 +0530

    pass or fail
    Signed-off-by: Harini03-11-2004 <harinim0311@gmail.com>

---
+++ pf.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class pf {
+    public static void main(String args[])
+    {
+
+        Scanner s = new Scanner(System.in);
+        int n=s.nextInt();
+        int marks=40;
+        if(marks>=40)
+        {
+            System.out.println("pass");
+        }
+        else{
+            System.out.println("fail");
+        }
+
+    }
+}
