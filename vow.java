5f8c58d9ac161884dc0846f2009c0d3b6e9a056e
Harini03-11-2004 <harinim0311@gmail.com>
2026-04-29 21:51:51 +0530

    vowels and consonent
    Signed-off-by: Harini03-11-2004 <harinim0311@gmail.com>

---
+++ vow.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class vow
+{
+    public static void main(String args[])
+    {
+        Scanner s =new Scanner(System.in);
+         char ch = s.next().toLowerCase().charAt(0);
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println("Vowel");
+        } else {
+            System.out.println("Consonant");
+        }
+    }
+}
