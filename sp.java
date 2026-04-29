6402aa7b29259cc2370a55138a4b52372196d741
Harini03-11-2004 <harinim0311@gmail.com>
2026-04-29 22:11:31 +0530

    simple printing program
    Signed-off-by: Harini03-11-2004 <harinim0311@gmail.com>

---
+++ sp.java
@@ -0,0 +1,10 @@
+public class sp {
+    public static void main(String arg[])
+        {
+            System.out.print("Hello");
+            System.out.println("World");
+            System.out.println("Welcome");
+            System.out.print("Welcome harini");
+        }
+}
+
