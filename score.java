ed5cd0d281259a0bb6b818838a9386952ae9dc2b
Harini03-11-2004 <harinim0311@gmail.com>
2026-04-29 11:22:11 +0530

    if else condition using score grading system
    Signed-off-by: Harini03-11-2004 <harinim0311@gmail.com>

---
+++ score.java
@@ -0,0 +1,37 @@
+public class score {
+    public static void main(String[] args) {
+        int score = 75;
+        if(score>=95)
+        {
+            System.out.println("Grade : o");
+        }
+        else if(score>=85)
+        {
+            System.out.println("Grade : A+");
+        }
+        else if(score>=70)
+        {
+            System.out.println("Grade : A");
+        }
+        else if(score>=65)
+        {
+            System.out.println("Grade : B+");
+        }
+        else if(score>=50)
+        {
+            System.out.println("Grade : B");
+        }
+        else if(score>=45)
+        {
+            System.out.println("Grade : c");
+        }
+        else if(score>=25)
+        {
+            System.out.println("Grade : D");
+        }
+        else if(score==0)
+        {
+            System.out.println("Grade : U");
+        }
+    }
+}
