c1c0b10a388d7f5d8a926cbc3a9741edd09a3870
Harini03-11-2004 <harinim0311@gmail.com>
2026-04-29 12:04:07 +0530

    Leap year System
    Signed-off-by: Harini03-11-2004 <harinim0311@gmail.com>

--- vote.java
+++ vote.java
@@ -1,6 +1,6 @@
 public class vote {
     public static void main(String[] args) {
-        int age = 18;
+        int age = 25;
         if(age>=18)
         {
             System.out.println("eligible to vote");
