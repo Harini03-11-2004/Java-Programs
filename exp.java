489fc4649801db5ed1660da82a359e7f387c0bbe
Harini03-11-2004 <harinim0311@gmail.com>
2026-05-01 14:03:57 +0530

    expense tracker
    Signed-off-by: Harini03-11-2004 <harinim0311@gmail.com>

---
+++ exp.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class exp {
+    public static void main(String args[])
+    {
+          expt obj=new expt();
+          obj.etrack();
+          
+}
+}
+class expt
+{
+    void etrack()
+    {
+        int total=0;
+        while(true){
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the expense name: ");
+        String a = s.nextLine();
+        System.out.println("Enter the expense price: ");
+        int b= s.nextInt();
+        System.out.println("continue the loop : ");
+        int c=s.nextInt();
+        total+=b;
+      if(c==0){
+        
+            break;
+        }
+    }
+    System.out.println("total expense : "+total);
+}
+}
+
+
+
+
