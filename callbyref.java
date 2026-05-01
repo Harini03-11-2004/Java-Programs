ea5b16b8e720920ab65341f99eb47e9906820f34
Harini03-11-2004 <harinim0311@gmail.com>
2026-05-01 20:48:56 +0530

    callbyref
    Signed-off-by: Harini03-11-2004 <harinim0311@gmail.com>

---
+++ callbyref.java
@@ -0,0 +1,13 @@
+class callbyref{
+    public static void main(String[] args){
+       method obj = new method();
+       obj.display(obj);
+    }
+}
+class method{
+    int a = 10;
+    static void display(method m){
+        System.out.println(m.a);
+    }  
+}
+
