1ac40debeb35d3ef23dbaeeff913ba25aa606518
Harini03-11-2004 <harinim0311@gmail.com>
2026-05-02 09:25:40 +0530

    First character
    Signed-off-by: Harini03-11-2004 <harinim0311@gmail.com>

---
+++ charlen.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class charlen {
+    public static void main(String args[])
+    {
+        Scanner s =new Scanner(System.in);
+        String a=s.nextLine();
+        System.out.println(a.charAt(0));
+        
+    }
+}
