45dd1365e43ee10e6d27298477f49dd327470ee6
Harini03-11-2004 <harinim0311@gmail.com>
2026-05-02 09:30:16 +0530

    startsWith function using check the string
    Signed-off-by: Harini03-11-2004 <harinim0311@gmail.com>

---
+++ startw.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class startw {
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        String a=s.nextLine();
+        if(a.startsWith("java"))
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("No");
+        }
+    }
+}
