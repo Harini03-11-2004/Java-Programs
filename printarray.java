27d64f5062823fd71147ea14e2bfe65f30e4bf1c
Harini03-11-2004 <harinim0311@gmail.com>
2026-05-02 11:42:48 +0530

    print the array without for loop
    Signed-off-by: Harini03-11-2004 <harinim0311@gmail.com>

---
+++ printarray.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+class printarray {
+    public static void main(String args[])
+    {
+        int arr[]=new int[5];
+        arr[0]=0;
+        arr[1]=1;
+        System.out.println(Arrays.toString(arr));
+}
+}
