5f2132c4d5222bfe63a6f4f81ac8215260a4cb75
Harini03-11-2004 <harinim0311@gmail.com>
2026-05-02 09:24:43 +0530

    String length
    Signed-off-by: Harini03-11-2004 <harinim0311@gmail.com>

---
+++ strlen.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class strlen {
+public static void main(String args[])
+{
+    Scanner s = new Scanner(System.in);
+    String n=s.nextLine();
+    if(n.length()<5)
+    {
+        System.out.println("Short");
+    }
+    else
+    {
+        System.out.println("long");
+    }
+}
+    
+}
